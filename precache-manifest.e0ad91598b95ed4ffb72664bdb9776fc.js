7a3de19854e2fe09d64bc94764793e8e1aa484ab
Anastasia <nastiyayesypenko@ukr.net>
2019-09-11 21:52:47 +0300

    Updates

---
+++ precache-manifest.e0ad91598b95ed4ffb72664bdb9776fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b3d67576abf7ea6da471ec2f814ab7",
+    "url": "/test-tasks/index.html"
+  },
+  {
+    "revision": "451705c4b905c7660c07",
+    "url": "/test-tasks/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "ca09bc2cba1d3cad1ad9",
+    "url": "/test-tasks/static/css/main.393c15a8.chunk.css"
+  },
+  {
+    "revision": "451705c4b905c7660c07",
+    "url": "/test-tasks/static/js/2.1503cae6.chunk.js"
+  },
+  {
+    "revision": "ca09bc2cba1d3cad1ad9",
+    "url": "/test-tasks/static/js/main.bc4d624f.chunk.js"
+  },
+  {
+    "revision": "fbac9570a945c1d97781",
+    "url": "/test-tasks/static/js/runtime~main.698265ab.js"
+  }
+]);
